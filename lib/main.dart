5753b2c13e5f20db738863da4bc4f839b31ad97f
MrWD10 <waleedakhtar484@gmail.com>
2021-07-27 09:59:26 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_practice2/common/theme.dart';
+import 'package:provider_practice2/models/cart.dart';
+import 'package:provider_practice2/models/catalog.dart';
+import 'package:provider_practice2/screens/cart.dart';
+import 'package:provider_practice2/screens/catalog.dart';
+import 'package:provider_practice2/screens/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Using MultiProvider is convenient when providing multiple objects.
+    return MultiProvider(
+      providers: [
+        // In this sample app, CatalogModel never changes, so a simple Provider
+        // is sufficient.
+        Provider(create: (context) => CatalogModel()),
+        // CartModel is implemented as a ChangeNotifier, which calls for the use
+        // of ChangeNotifierProvider. Moreover, CartModel depends
+        // on CatalogModel, so a ProxyProvider is needed.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MyLogin(),
+          '/catalog': (context) => MyCatalog(),
+          '/cart': (context) => MyCart(),
+        },
+      ),
+    );
+  }
+}
